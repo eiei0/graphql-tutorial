ca370f4ca16fe2fa9e6e8da51c9fc9c3437ac911
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2021-12-08 00:37:14 -0600

    Run migrations to create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_052439) do
+ActiveRecord::Schema.define(version: 2021_12_08_063603) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
